a37cfc254c64211cbfd01b67e21aeffa1f881c0b
Lorenzo Gandino <lorenzoga94@gmail.com>
2021-03-24 00:53:02 +0100

    add commet in sql file

--- commons-db-utils-h2/src/test/resources/h2-dml.sql
+++ commons-db-utils-h2/src/test/resources/h2-dml.sql
@@ -1,3 +1,7 @@
+-- START INSERT HelloWorld --
+
 INSERT INTO HelloWorld VALUES (1, 'Hello, World!');
 INSERT INTO HelloWorld VALUES (2, 'Hello, World! 2');
 INSERT INTO HelloWorld VALUES (3, 'Hello, World! 3');
+
+-- END INSERT HelloWorld --
